624e38547c9173aeba2801e291b8539dadedf7a8
zzz-creator <84800058+zzz-creator@users.noreply.github.com>
2025-07-18 07:38:07 -0400

    Update about.ts

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -6,119 +6,18 @@ export const DEVELOPERS: Array<IPerson> = [
     link: "https://github.com/Kedyn",
   },
   {
-    name: "Francis Tse",
-    link: "https://github.com/francistse23",
-  },
-  {
-    name: "Daniel Singer",
-    link: "https://github.com/chilblane",
-  },
-  {
-    name: "Kevin Han",
-    link: "https://github.com/kevinydhan",
-  },
-  {
-    name: "Alena Choong",
-    link: "https://github.com/leeeennyy",
-  },
-  {
-    name: "Griffin Suparto",
-    link: "https://github.com/Viou",
-  },
-  {
-    name: "Pawel Boguslawski",
-    link: "https://github.com/bogusweb",
-  },
+    name: "zzz-creator",
+    link: "https://github.com/zzz-creator/",
+}
+  
 ];
 
 export const TRANSLATORS: Array<IPerson> = [
-  {
-    name: "Coleyra",
-    link: "https://github.com/Coleyra",
-  },
-  {
-    name: "Danilo Moura",
-    link: "https://github.com/danilolmoura",
-  },
-  {
-    name: "Fabian9799",
-    link: "https://github.com/fabian9799",
-  },
-  {
-    name: "Eduardo Vega",
-    link: "https://github.com/eduardovegar",
-  },
-  {
-    name: "Parsa Eskandarnejad",
-    link: "https://github.com/parsaaes",
-  },
-  {
-    name: "Kinishina",
-    link: "https://github.com/Kinishina",
-  },
-  {
-    name: "Victor Pietro",
-    link: "https://github.com/pietroow",
-  },
-  {
-    name: "Michał Stankiewicz",
-    link: "http://fb.niezwyczajniezwyczajny.pl",
-  },
-  {
-    name: "Drison64",
-    link: "https://github.com/Drison64",
-  },
-  {
-    name: "Itay Kabalo",
-    link: "https://twitter.com/itaykabalo",
-  },
-  {
-    name: "Unviray",
-    link: "https://github.com/Unviray",
-  },
-  {
-    name: "Nuno Lopes",
-    link: "",
-  },
-  {
-    name: "Jarsa132",
-    link: "https://github.com/Jarsa132",
-  },
-  {
-    name: "Chee Murakami",
-    link: "https://github.com/cheemurakami",
-  },
-  {
-    name: "tokkiyaa",
-    link: "https://github.com/tokkiyaa",
-  },
-];
 
+  ];
+  
 export const ARTISTS: Array<IPerson> = [
-  {
-    name: "HoneyLemonDaisy",
-    link: "https://twitter.com/honeylemondaisy",
-  },
-  {
-    name: "Stark_001001",
-    link: "https://www.reddit.com/user/Stark_001001",
-  },
-  {
-    name: "mooseknuckle_king",
-    link: "https://www.reddit.com/user/mooseknuckle_king/",
-  },
-  {
-    name: "Caburum",
-    link: "https://among-us.fandom.com/wiki/User:Caburum",
-  },
-  {
-    name: "F-sharp",
-    link: "https://among-us.fandom.com/wiki/User:F-sharp",
-  },
-  {
-    name: "VEGANS-SavageSlayer",
-    link: "https://among-us.fandom.com/wiki/User:VEGANS-SavageSlayer",
-  },
+ 
 ];
 
 export const SPECIAL: Array<IPerson> = [
@@ -126,4 +25,5 @@ export const SPECIAL: Array<IPerson> = [
     name: "fuslie",
     link: "https://www.twitch.tv/fuslie",
   },
+  
 ];
