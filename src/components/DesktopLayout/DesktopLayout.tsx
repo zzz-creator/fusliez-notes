4b054adeafdfcc5546568ad9f58e8e1894ea8f1f
zzz-creator <84800058+zzz-creator@users.noreply.github.com>
2025-12-08 16:16:41 -0500

    Change footer link to 'about:blank'
    Updated footer link to open a blank page instead of a Google Form.
    
    Signed-off-by: zzz-creator <84800058+zzz-creator@users.noreply.github.com>

--- src/components/DesktopLayout/DesktopLayout.tsx
+++ src/components/DesktopLayout/DesktopLayout.tsx
@@ -89,7 +89,7 @@ export default function DesktopLayout(): JSX.Element {
         </a>{" "}
         |{" "}
         <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSdrIUKoatKPmYTq1pxzX1jNtM9mYrBmG8sfkJFfl8NZ6EbjuA/viewform"
+          href="about:blank"
           target="_blank"
           rel="noopener noreferrer"
         >
